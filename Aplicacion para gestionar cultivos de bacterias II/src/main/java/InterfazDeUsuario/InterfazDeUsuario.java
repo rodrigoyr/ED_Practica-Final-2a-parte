780fd3ff788c8477d28fbc199aea8f697d0b3168
rodrigoyr <ryeperub@myuax.com>
2024-05-15 10:32:07 +0200

    commit

---
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/InterfazDeUsuario/InterfazDeUsuario.java
@@ -0,0 +1,24 @@
+package InterfazDeUsuario;
+
+import Experimento.Experimento;
+import PoblacionDeBacterias.Poblacion;
+import GestionDeArchivos.GestorDeArchivos;
+import Simulacion.Simulacion;
+
+import java.util.Scanner;
+
+public class InterfazDeUsuario {
+    private Scanner scanner;
+
+    public InterfazDeUsuario() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void iniciar() {
+        // Aquí se implementará la lógica para interactuar con el usuario
+        // Por ejemplo, mostrar un menú, recibir la entrada del usuario, etc.
+    }
+
+    // Métodos adicionales para interactuar con el usuario
+    // ...
+}
