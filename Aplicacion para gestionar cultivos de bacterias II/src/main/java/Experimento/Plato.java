9cc042835f3bd90b774bf2a3c8d5addcf8d87300
rodrigoyr <ryeperub@myuax.com>
2024-05-15 13:30:47 +0200

    commit

--- Aplicacion para gestionar cultivos de bacterias II/src/main/java/Experimento/Plato.java
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/Experimento/Plato.java
@@ -1,4 +1,17 @@
 package Experimento;
 
 public class Plato {
+    private int[][] celdas;
+
+    public Plato(int size) {
+        this.celdas = new int[size][size];
+    }
+
+    public void inicializar() {
+        // Implementar la lógica para inicializar el plato
+    }
+
+    public void distribuirComida() {
+        // Implementar la lógica para distribuir la comida
+    }
 }
