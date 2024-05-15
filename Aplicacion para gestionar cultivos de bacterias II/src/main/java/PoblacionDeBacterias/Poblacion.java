9f9da240c8bb9cf3390fcec845de22c667acaf66
rodrigoyr <ryeperub@myuax.com>
2024-05-15 10:59:47 +0200

    commit

---
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/PoblacionDeBacterias/Poblacion.java
@@ -0,0 +1,17 @@
+package PoblacionDeBacterias;
+
+public class Poblacion {
+    private int numeroBacteriasIniciales;
+
+    public Poblacion(int numeroBacteriasIniciales) {
+        this.numeroBacteriasIniciales = numeroBacteriasIniciales;
+    }
+
+    public int getNumeroBacteriasIniciales() {
+        return this.numeroBacteriasIniciales;
+    }
+
+    public void setNumeroBacteriasIniciales(int numeroBacteriasIniciales) {
+        this.numeroBacteriasIniciales = numeroBacteriasIniciales;
+    }
+}
