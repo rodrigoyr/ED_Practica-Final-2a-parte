47733d307894ef932c2edad6b087f29596ec11a4
rodrigoyr <ryeperub@myuax.com>
2024-05-15 13:51:41 +0200

    commit

--- Aplicacion para gestionar cultivos de bacterias II/src/main/java/PoblacionDeBacterias/Poblacion.java
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/PoblacionDeBacterias/Poblacion.java
@@ -10,17 +10,35 @@ public class Poblacion {
     private Date fechaInicio;
     private List<Bacteria> bacterias;
 
-    // Tus otros métodos aquí...
+    public Poblacion(String nombre, Date fechaInicio, List<Bacteria> bacterias) {
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.bacterias = bacterias;
+    }
 
     public Date getFechaInicio() {
         return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public List<Bacteria> getBacterias() {
         return bacterias;
     }
+
+    public void setBacterias(List<Bacteria> bacterias) {
+        this.bacterias = bacterias;
+    }
+
+    // Tus otros métodos aquí...
 }
