8babfb41e45999f980a22f98b6756c09ac7e4169
rodrigoyr <ryeperub@myuax.com>
2024-05-15 10:59:39 +0200

    commit

--- Aplicacion para gestionar cultivos de bacterias II/src/main/java/Experimento/Experimento.java
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/Experimento/Experimento.java
@@ -7,31 +7,18 @@ import java.util.Comparator;
 
 public class Experimento {
     private String nombre;
-    private List<Poblacion> poblaciones;
+    private Poblacion poblacion;
 
-    public Experimento(String nombre) {
-        this.nombre = nombre;
-        this.poblaciones = new ArrayList<>();
+    public Experimento(Poblacion poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public Poblacion getPoblacion() {
+        return this.poblacion;
     }
 
     public void agregarPoblacion(Poblacion poblacion) {
-        this.poblaciones.add(poblacion);
-    }
-
-    public void eliminarPoblacion(Poblacion poblacion) {
-        this.poblaciones.remove(poblacion);
-    }
-
-    public void ordenarPoblacionesPorFecha() {
-        this.poblaciones.sort(Comparator.comparing(Poblacion::getFechaInicio));
-    }
-
-    public void ordenarPoblacionesPorNombre() {
-        this.poblaciones.sort(Comparator.comparing(Poblacion::getNombre));
-    }
-
-    public void ordenarPoblacionesPorNumeroDeBacterias() {
-        this.poblaciones.sort(Comparator.comparing(Poblacion::getNumeroBacteriasIniciales));
+        this.poblacion = poblacion;
     }
 
     // Métodos adicionales para guardar y cargar experimentos desde archivos
