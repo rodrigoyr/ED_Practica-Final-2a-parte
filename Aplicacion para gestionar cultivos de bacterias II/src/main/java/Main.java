593c2a17c29945bd41f2bf6ea0a932d063f5d50d
rodrigoyr <ryeperub@myuax.com>
2024-05-15 10:34:41 +0200

    commit

---
+++ Aplicacion para gestionar cultivos de bacterias II/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import InterfazDeUsuario.InterfazDeUsuario;
+
+public class Main {
+    public static void main(String[] args) {
+        InterfazDeUsuario interfazDeUsuario = new InterfazDeUsuario();
+        interfazDeUsuario.iniciar();
+    }
+}
